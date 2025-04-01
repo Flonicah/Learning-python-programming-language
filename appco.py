0fcaddef68377423a337c39aefdf891d6896732d
Flonicah Leonard <flonicahphiri@gmail.com>
2025-04-01 10:02:33 +0200

    getting deep in knowing the basics of python language

---
+++ appco.py
@@ -0,0 +1,29 @@
+#continuation of video learning python lesson
+# getting access to more function by importing them
+
+
+# importing math
+from math import * # allow to do more with math funtion and more
+my_num = -5
+print(floor(3.7))# gets the lower number 
+print()
+print(ceil(3.7))#gets the highest number of it
+
+print()
+print(sqrt(36))# the square roo
+
+#check more on math functions in python
+
+
+#getting inputs from a user into the program
+
+print("Getting input fron the user")
+print()
+
+name = input("What is your Name; ")
+print()
+print("Hello " + name + "!")
+print()
+age = input("How old are you? ")
+print()
+print("Wow! " + name ,", so you are " + age + " years.")
