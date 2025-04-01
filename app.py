9fc31f3ecbe212b59766308236c996a01a3db9de
Flonicah Leonard <flonicahphiri@gmail.com>
2025-04-01 09:40:43 +0200

    python lesson from tutorials first part

---
+++ app.py
@@ -0,0 +1,100 @@
+#First program about hello world
+print("WELCOME TO THE WORLD OF CODING IN PYTHON LANGUAGE")
+print()
+#Variable and Data Type
+#creating variables
+    
+character_name = "John"
+character_age = "35"
+print("Thre once was a man named " +character_name  +",")
+print("He was " + character_age + " years old.")
+character_age = "70"
+print("He really liked the name " + character_name + ",")
+print("but he didn't like being" + character_age + ".")
+
+#data type : strings, numbers, binary
+
+leo = 70 #this is the number data type with no need of the quote decimal or more floats
+
+isMale = True # the binary data type
+
+y = "leonard" # the strings type
+
+#WORKING WITH STRINGS ; Playing tasks
+
+print()
+print("STRINGS TASKS\n")
+print("Giraffe Academy")
+#using the \ to either print next line or tell to print a certain character that is special in a astring
+print("I love my mother so much \"blessed\"")
+
+#creating a string variable
+print()
+
+
+phrase ="Leonard Academy"
+
+print(phrase + "is cool")#used plus for concatenation
+
+#using special things in this case a function
+print()
+print(phrase.lower())# converting to lower case
+print()
+print(phrase.upper())#converting to uper case here
+print()
+print(phrase.isupper()) # checking if true or fale binary case
+print()
+print(phrase.islower()) # checking if it is all lower case
+print()
+print("All is going well as of now")
+
+print()
+#using functions in combination with each other
+print(phrase.upper().isupper())# first is converts it to upper and  checks it to see if it is upper case
+print()
+#use the length fnction to check the length of the string
+print(len(phrase))
+#getting individual character in a string
+print()
+print(phrase[2]) # the character storation is index to 0 zero
+
+# working with the index function
+print(phrase.index("e"))
+print()
+print(phrase.index("Acad"))
+
+# working with replace function
+print()
+print(phrase.replace("Academy", "Vericah"))# just a few functions in python with strings
+
+print()
+
+#working with numbers in python
+print("WORKING WITH NUMBERS")
+
+print()
+#using basic arithment operations like add,/ and more math
+print(2*3)
+print()
+print(3*(4+5))
+print()
+print(10 % 3)# the module function which gives the remender
+print()
+#converting a number to string
+my_num = 5
+print(str(my_num) + "is my fav number")
+# common function related to number
+#the abs = absolute value
+print(abs(my_num))
+print()
+print(pow(3,2)) # the power function related to math
+print()
+print(max(5,9))# using the max, min too
+print(round(3.094))
+
+print("continuation in appco file of the lesson in python")
+
+ 
+
+
+
