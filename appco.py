2847cb379af2ffc42be58e6174c7c6259bbfc4a1
Flonicah Leonard <flonicahphiri@gmail.com>
2025-04-01 10:25:57 +0200

    basic building of the calculator with getting the input from the user eith sfloats or integers

--- appco.py
+++ appco.py
@@ -27,3 +27,30 @@ print()
 age = input("How old are you? ")
 print()
 print("Wow! " + name ,", so you are " + age + " years.")
+
+
+#Building a basic Calculator
+print("BUILDING A BASIC CALCULATOR")
+print()
+# getting numbers
+num1 = input("Enter the first number; ")
+num2 = input("Enter the second number; ")
+result = num1 + num2
+print(result) # it only concanate the without doing any basic math operations but takes in the input as strings
+
+print()
+print("dealing with numbers input to do the math opreations /function tasks")
+
+#getting number with special py functions
+#literals int and floats
+# the int use first
+num3 = input("Enter the  number; ")
+num4 = input("Enter the another number; ")
+results = int(num3) + int(num4)
+print(results)
+
+# using the float function now
+num5 = input("Enter the first number; ")
+num6 = input("Enter the second number; ")
+resulted = float(num5)+ float(num6)
+print(resulted)
