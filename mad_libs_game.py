fc375f3ba5b6c82a370eaac6834521c40554da37
Flonicah Leonard <flonicahphiri@gmail.com>
2025-04-01 10:46:16 +0200

    The mad libs game

---
+++ mad_libs_game.py
@@ -0,0 +1,30 @@
+#mad libs game
+# building a mad libs game
+print()
+print("The initial Program")
+print("Roses are red")
+print("Voilets are blue")
+print("I love you")
+print()
+print("where it will be edited sample")
+print()
+
+# getting to work on it now the initial poem
+print("Roses are {color}")
+print("{plural noun} are blue")
+print("I love {celebrity}")
+
+# going about the libs game
+print()
+print("The Actual final work now")
+print()
+color = input("Enter a color; ")
+plural_noun = input("Enter a plural noun; ")
+celebrity = input("Enter a celebrity; ")
+# working on them now
+print()
+print("Results")
+print()
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love "+ celebrity)
