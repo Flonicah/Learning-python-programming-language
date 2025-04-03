ea75a69771b1f9d0c83077113127ba56806e4b5e
Flonicah Leonard <flonicahphiri@gmail.com>
2025-04-03 13:06:39 +0200

    while loop work

--- whilel_oops.py
+++ whilel_oops.py
@@ -1,1 +1,10 @@
 # dealing with the while loops in python languages
+# enable execute a block of code repeatedly till it is true
+
+i =1
+# creating the while loop
+while i <= 10:
+    print(i)
+    i = i +1
+
+print("Done with the loop line")
