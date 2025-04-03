22856170634bc6f5c0c398da8f12fc498c7cb3a4
Flonicah Leonard <flonicahphiri@gmail.com>
2025-04-03 11:14:29 +0200

    worked on buillding the calculator with using conditions

--- buildingabettercalculator.py
+++ buildingabettercalculator.py
@@ -1,1 +1,25 @@
 #building a better calculator with python
+num1 = float(input ("Enter the first number: "))
+op = input ("Enter the operator: ")
+num2 = float(input ("Enter the second number: "))
+
+
+if op == "+": # addition operator
+    print(num1 + num2)
+    
+elif op == "-":# minus operator
+    print(num1 - num2)
+    
+elif op == "/":#division operator
+    print(num1 / num2)
+    
+elif op == "*":#multiplication operator
+    print(num1 * num2)
+    
+elif op == "%":# remender operator
+    print(num1 % num2)
+else:
+    print("Invalid operator ")
+
+
+#look for more on this too
